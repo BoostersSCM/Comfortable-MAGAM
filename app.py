3610f66244790667065935b6eeff0d3b44b62ff5
BoostersSCM <ku.do@boosters.kr>
2025-12-26 11:43:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ from authlib.integrations.requests_client import OAuth2Session
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
